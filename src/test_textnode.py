70b77a9a0fcca0f42bba9a6dba9265185ad18b3d
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-04-17 19:18:06 +0200

    ch3 done

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -41,5 +41,107 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.to_html(), '<a href="test.com">This is a text node</a>')
 
+    def test_delimiter1(self):
+        node = TextNode('Hi, split this **bold** text please', TextType.TEXT)
+        out = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(f'{out}', '[TextNode(Hi, split this , TextType.TEXT, None), TextNode(bold, TextType.BOLD, None), TextNode( text please, TextType.TEXT, None)]')
+
+    #def test_delimiter2(self):
+        #node = TextNode('Hi, split this **bold** text please', TextType.TEXT)
+        #with self.assertRaises(Exception, msg="Delimiter does is not in the Node"):
+        #    split_nodes_delimiter([node], "__", TextType.BOLD)
+    
+    def test_delimiter3(self):
+        node = TextNode('Hi, split this **bold** text** please', TextType.TEXT)
+        with self.assertRaises(Exception, msg="Not enough splits"):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_delimiter4(self):
+        node = TextNode('Hi, split this **bold text please', TextType.TEXT)
+        with self.assertRaises(Exception, msg="Not enough splits"):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+    def test_delimiter5(self):
+        node = TextNode('**Hi**, split this bold text please', TextType.TEXT)
+        out = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(f'{out}', '[TextNode(Hi, TextType.BOLD, None), TextNode(, split this bold text please, TextType.TEXT, None)]')
+    
+    def test_delimiter5(self):
+        node = TextNode('**Hi**, split this bold text please', TextType.TEXT)
+        node1 = TextNode('Hi, split this **bold** text please', TextType.TEXT)
+        out = split_nodes_delimiter([node, node1], "**", TextType.BOLD)
+        self.assertEqual(f'{out}', '[TextNode(Hi, TextType.BOLD, None), TextNode(, split this bold text please, TextType.TEXT, None), TextNode(Hi, split this , TextType.TEXT, None), TextNode(bold, TextType.BOLD, None), TextNode( text please, TextType.TEXT, None)]')
+    
+    def test_split_images(self):
+        node = TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png")
+            ],
+            new_nodes,
+        )
+
+    def test_split_link(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
+            ],
+            new_nodes,
+        )
+
+    def test_split_link1(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev)[to youtube](https://www.youtube.com/@bootdotdev) hello", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+                TextNode(" hello", TextType.TEXT)
+            ],
+            new_nodes,
+        )
+
+    def test_split_link2(self):
+        node = TextNode("[to boot dev](https://www.boot.dev)[to youtube](https://www.youtube.com/@bootdotdev) hello", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+                TextNode(" hello", TextType.TEXT)
+            ],
+            new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        out = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            out
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,7 @@
 
 from enum import Enum
 from htmlnode import *
+from functions import extract_markdown_images, extract_markdown_links
 
 class TextType(Enum):
     TEXT = "t"
@@ -26,6 +27,80 @@ def text_node_to_html_node(text_node):
             return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Format not supported.")
+
+#ch3 l1       
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    #old notes = list of TextNodes, text_type is enum
+    new_nodes = []
+    for node in old_nodes:
+        if delimiter not in node.text:
+            new_nodes.append(node)
+            continue
+            #raise Exception("Delimiter does is not in the Node")
+        spl = node.text.split(delimiter)
+        if (len(spl) - 1) % 2 != 0:
+            raise Exception("Not enough splits")
+        for i in range(0, len(spl)):
+            if len(spl[i]) == 0:
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(spl[i], node.text_type, node.url))
+            else:
+                new_nodes.append(TextNode(spl[i], text_type, node.url))
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        ext = extract_markdown_images(node.text)
+        if not ext:
+            new_nodes.append(node)
+            continue
+            #if ext is nothing, old node is added to new_nodes
+        text = node.text
+        for img, link in ext:
+            spl = text.split(f"![{img}]({link})", 1)
+            if spl[0]:
+                new_nodes.append(TextNode(spl[0], node.text_type, node.url))
+            new_nodes.append(TextNode(img, TextType.IMAGE, link))
+            #sets text to last string to perform additional splits 
+            text = spl[-1]
+        #once this loop ends, create the last object with remaining str - text
+        if text:
+            new_nodes.append(TextNode(text, node.text_type, node.url))
+    return new_nodes
+            
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        ext = extract_markdown_links(node.text)
+        if not ext:
+            new_nodes.append(node)
+            continue
+        
+        text = node.text
+        for link_text, link_url in ext:
+            spl = text.split(f"[{link_text}]({link_url})", 1)
+            if spl[0]:
+                new_nodes.append(TextNode(spl[0], node.text_type, node.url))
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            text = spl[-1]
+            
+        if text:
+            new_nodes.append(TextNode(text, node.text_type, node.url))
+    return new_nodes
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    nodes = [node]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
