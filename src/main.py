3eec45b2438a5df95a6c90bf8fdc3721eb425bdc
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-05-04 20:07:29 +0200

    final build try

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from textnode import *
 from modules import *
+import sys
+
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+
+def get_base_path():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return "/"
+
+basepath = get_base_path()
 
 def main():
     print("Deleting public directory...")
@@ -15,6 +25,6 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
--- src/modules.py
+++ src/modules.py
@@ -17,17 +17,17 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
         else:
             copy_files_recursive(from_path, dest_path)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
     
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
 
     from_file = open(from_path, "r")
@@ -43,6 +43,7 @@ def generate_page(from_path, template_path, dest_path):
     html_str = markdown_parrent.to_html()
 
     template = template.replace("{{ Title }}", title).replace("{{ Content }}", html_str)
+    template = template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
